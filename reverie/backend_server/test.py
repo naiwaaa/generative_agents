4557de90a061d4b77ad54620e5ceac30a9605dfd
Tho Nguyen <contact@naiwaaa.simplelogin.com>
2024-12-08 01:59:32 +0700

    style: format with pre-commit

--- reverie/backend_server/test.py
+++ reverie/backend_server/test.py
@@ -3,6 +3,7 @@
 File: gpt_structure.py
 Description: Wrapper functions for calling OpenAI APIs.
 """
+
 from __future__ import annotations
 
 import openai
@@ -42,18 +43,18 @@ prompt = """
 Character 1: Maria Lopez is working on her physics degree and streaming games on Twitch to make some extra money. She visits Hobbs Cafe for studying and eating just about everyday.
 Character 2: Klaus Mueller is writing a research paper on the effects of gentrification in low-income communities.
 
-Past Context: 
+Past Context:
 138 minutes ago, Maria Lopez and Klaus Mueller were already conversing about conversing about Maria's research paper mentioned by Klaus This context takes place after that conversation.
 
 Current Context: Maria Lopez was attending her Physics class (preparing for the next lecture) when Maria Lopez saw Klaus Mueller in the middle of working on his research paper at the library (writing the introduction).
 Maria Lopez is thinking of initating a conversation with Klaus Mueller.
 Current Location: library in Oak Hill College
 
-(This is what is in Maria Lopez's head: Maria Lopez should remember to follow up with Klaus Mueller about his thoughts on her research paper. Beyond this, Maria Lopez doesn't necessarily know anything more about Klaus Mueller) 
+(This is what is in Maria Lopez's head: Maria Lopez should remember to follow up with Klaus Mueller about his thoughts on her research paper. Beyond this, Maria Lopez doesn't necessarily know anything more about Klaus Mueller)
 
-(This is what is in Klaus Mueller's head: Klaus Mueller should remember to ask Maria Lopez about her research paper, as she found it interesting that he mentioned it. Beyond this, Klaus Mueller doesn't necessarily know anything more about Maria Lopez) 
+(This is what is in Klaus Mueller's head: Klaus Mueller should remember to ask Maria Lopez about her research paper, as she found it interesting that he mentioned it. Beyond this, Klaus Mueller doesn't necessarily know anything more about Maria Lopez)
 
-Here is their conversation. 
+Here is their conversation.
 
 Maria Lopez: "
 ---
